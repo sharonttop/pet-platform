79e1fc73056c7d8a80d82bfdc0323a8acf1b5b7a
sharonttop <sharonttop@gmail.com>
2021-08-21 18:00:58 +0800

    整合組長的navbar

--- proj/partials/navbar.php
+++ proj/partials/navbar.php
@@ -1,19 +1,32 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand" href="#">寵物整合平台</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
+         <a class="navbar-brand" href="index_.php">HOME<i class="fas fa-paw"></i></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
 
         <div class="collapse navbar-collapse mt-2" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="customers-list.php">客戶資料</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">毛孩找家</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">毛孩旅宿</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">貓貓用品</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">狗狗用品</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="article-list.php">鏟屎官大補帖</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="customers-list.php">客戶資料</a>
+                    </li>
             </ul>
-
-
+    
             <ul class="navbar-nav">
                 <?php if(isset($_SESSION['user'])): ?>
                     <li class="nav-item active">
@@ -40,8 +53,11 @@
                         <a class="nav-link" href="member-insert.php">註冊</a>
                     </li>
                 <?php endif; ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i></a>
+                    </li>
             </ul>
 
         </div>
-  </div>
+
 </nav>
